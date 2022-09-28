1f993299a6a8a052354db3feb652da49920f5f4d
Ali <alifeyza4017@gmail.com>
2022-09-28 09:20:42 +0300

    added

--- app.js
+++ app.js
@@ -19,5 +19,55 @@ btn.addEventListener("click", ()=> {
         const url = `https://api.edamam.com/search?q=${recipe}&app_id=${appID}&app_key=${api_key}`;
         console.log(url)
 
-        fetch(url).then((res)=> console.log(res))
+        fetch(url).then(function(res) {
+            if(!res.ok) {
+                // renderError()
+                throw new Error(`Something went wrong ${res.status}`)
+            }
+            return res.json()
+        }).then((data) =>  {
+            renderRecipe(data)
+        }).catch((err)=> console.log(err))
     }
+
+       const  renderRecipe = (recipes) => {
+        console.log(recipes)
+        const cardDiv = document.querySelector("#card")
+
+        console.log(recipes.hits[0].recipe)
+
+        cardDiv.innerHTML = `
+         <img " src="${
+           recipes.hits[0].recipe.image
+         }" class="card-img-top " alt="...">
+        <div class="card-flex d-flex flex-lg-column-reverse ">
+            <div class="felxcard ">
+                <div class="card-body">
+                    <h5 class="card-title">${recipes.q}</h5>
+                </div>
+                <ul class="list-group d-flex justify-content-start list-unstyled ">
+                    <li class="list ">Calories: <span>${recipes.hits[0].recipe.calories.toFixed(
+                      2
+                    )}</span></li>
+                    <li class="list">Diet Label: <span>${
+                      recipes.hits[0].recipe.dietLabels
+                    }</span></li>
+                    <li class="list">Healt Label: <span>${
+                      recipes.hits[0].recipe.healthLabels[0]
+                    }, ${recipes.hits[0].recipe.healthLabels[1]}</span></li>
+                </ul>
+            </div>
+            <div class="card-body">
+                <button><a href="${recipes.hits[0].recipe.url}" target="_blank" class="card-link p-2">View Recipe</a></button>
+            </div>
+        </div>
+        
+        `;
+       }
+
+
+
+
+
+
+    fetchRecipe("Baked Chicked")
